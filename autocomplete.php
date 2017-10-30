b87905419fa618747c1d5cd8aff097fc80a2594c
Iqbal <iqbalatmadja@gmail.com>
2017-10-30 13:53:38 +0700

    added autocomplete

--- autocomplete.php
+++ autocomplete.php
@@ -69,3 +69,71 @@ var options = {
 
 $("#provider-xml").easyAutocomplete(options);
 </script>
+<hr/>
+<input id="template-desc" /> Template "description"
+<script>
+var options = {
+	data: [ {name: "Avionet", type: "air", icon: "http://lorempixel.com/100/50/transport/2"},
+		{name: "Car", type: "ground", icon: "http://lorempixel.com/100/50/transport/8"},
+		{name: "Motorbike", type: "ground", icon: "http://lorempixel.com/100/50/transport/10"},
+		{name: "Plane", type: "air", icon: "http://lorempixel.com/100/50/transport/1"},
+		{name: "Train", type: "ground", icon: "http://lorempixel.com/100/50/transport/6"}],
+
+	getValue: "name",
+
+	template: {
+		type: "description",
+		fields: {
+			description: "type"
+		}
+	}
+};
+
+$("#template-desc").easyAutocomplete(options);
+</script>
+<hr/>
+<input id="template-icon-left" /> Template icon-left
+<script>
+var options = {
+	data: [ {name: "Avionet", type: "air", icon: "http://lorempixel.com/100/50/transport/2"},
+		{name: "Car", type: "ground", icon: "http://lorempixel.com/100/50/transport/8"},
+		{name: "Motorbike", type: "ground", icon: "http://lorempixel.com/100/50/transport/10"},
+		{name: "Plane", type: "air", icon: "http://lorempixel.com/100/50/transport/1"},
+		{name: "Train", type: "ground", icon: "http://lorempixel.com/100/50/transport/6"}],
+
+	getValue: "name",
+
+	template: {
+		type: "iconLeft",
+		fields: {
+			iconSrc: "icon"
+		}
+	}
+};
+
+$("#template-icon-left").easyAutocomplete(options);
+</script>
+<hr/>
+<input id="template-icon-right" /> Template icon-right
+<script>
+var options = {
+	data: [ {name: "Avionet", type: "air", icon: "http://lorempixel.com/100/50/transport/2"},
+		{name: "Car", type: "ground", icon: "http://lorempixel.com/100/50/transport/8"},
+		{name: "Motorbike", type: "ground", icon: "http://lorempixel.com/100/50/transport/10"},
+		{name: "Plain", type: "air", icon: "http://lorempixel.com/100/50/transport/1"},
+		{name: "Train", type: "ground", icon: "http://lorempixel.com/100/50/transport/6"}],
+
+
+	getValue: "name",
+
+	template: {
+		type: "iconRight",
+		fields: {
+			iconSrc: "icon"
+		}
+	}
+};
+
+$("#template-icon-right").easyAutocomplete(options);
+</script>
+
